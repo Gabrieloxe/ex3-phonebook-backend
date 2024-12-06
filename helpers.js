4e5c2eee0cefdf6206bb1faa03b02091b49b05d1
Gabriel Ong <gabrielongxe@gmail.com>
2024-12-06 16:09:09 +0800

    add crud methods and morgan middleware

---
+++ helpers.js
@@ -0,0 +1,25 @@
+const generateId = () => {
+  let id = Math.floor(Math.random() * 1000000);
+  const numbers = persons.map(p => p.id);
+  while (numbers.includes(id)) {
+    id = Math.floor(Math.random() * 1000000);
+  }
+  return id;
+};
+
+const validatePerson = person => {
+  if (
+    !person.name ||
+    !person.number ||
+    person.name === '' ||
+    person.number === ''
+  ) {
+    return true;
+  }
+  return false;
+};
+
+module.exports = {
+  generateId,
+  validatePerson,
+};
--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const { generateId, validatePerson } = './helpers';
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+morgan.token('body', req => {
+  if (req.body) {
+    return JSON.stringify(req.body);
+  }
+});
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 const persons = [
   {
-    id: '1',
+    id: 1,
     name: 'Arto Hellas',
     number: '040-123456',
   },
   {
-    id: '2',
+    id: 2,
     name: 'Ada Lovelace',
     number: '39-44-5323523',
   },
   {
-    id: '3',
+    id: 3,
     name: 'Dan Abramov',
     number: '12-43-234345',
   },
   {
-    id: '4',
+    id: 4,
     name: 'Mary Poppendieck',
     number: '39-23-6423122',
   },
 ];
-
-app.get('/api/persons', (req, res) => {
-  res.json(persons);
-});
 
 app.get('/info', (request, response) => {
   const info = `Phonebook has info for ${
@@ -35,6 +51,80 @@ app.get('/info', (request, response) => {
 
   response.send(info);
 });
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (validatePerson(body)) {
+    response.status(400).json({
+      error: 'Either the name or number is missing',
+    });
+  }
+
+  const names = persons.map(p => p.name);
+  if (names.includes(body.name)) {
+    response.status(400).json({
+      error: 'name must be unique',
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(p => p.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.put('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const idExist = persons.find(p => p.id === id);
+
+  if (!idExist) {
+    response.status(404).end();
+  }
+
+  const body = request.body;
+
+  if (validatePerson(body)) {
+    return response.status(400).json({
+      error: 'Either the name or number is missing',
+    });
+  }
+
+  persons = persons.filter(p => p.id !== id);
+  const person = {
+    id,
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(p => p.id !== id);
+  response.status(204).end();
+});
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
